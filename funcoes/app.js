be5e6ec73ca875dbb62585d4eecb92c27a0ebda0
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-12-13 17:09:36 -0300

    Funções

---
+++ funcoes/app.js
@@ -0,0 +1,6 @@
+function calculadoraAreaTerreno (largura, comprimento){
+    var area = largura * comprimento
+    return area
+}
+
+document.write("O terreno possui " + calculadoraAreaTerreno(15, 25) + " metros quadrados")
