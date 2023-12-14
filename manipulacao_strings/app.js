c7a06cafb0e261251fcea842c09e5c79639e1126
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-12-14 16:41:12 -0300

    Manipulação de Strings

---
+++ manipulacao_strings/app.js
@@ -0,0 +1,20 @@
+//var strings = "Eduardo Cesar Maia Moreira Oliveira"
+//document.write(strings.charAt(4))
+
+//var nome = "Eduardo Cesar Maia Moreira Oliveira"
+//document.write(nome.indexOf("d"))
+
+//var nome = "Eduardo Cesar Maia Moreira Oliveira"
+//document.write(nome.replace("Cesar", "César"))
+
+//var nome = "Eduardo Cesar Maia Moreira Oliveira"
+//document.write(nome.substr(8, 5)) // 8 index da primeira letra | 5 quantidade de casas adiante.
+
+//var nome = "Eduardo Cesar Maia Moreira Oliveira"
+//document.write(nome.toUpperCase())
+
+//var nome = "Eduardo Cesar Maia Moreira Oliveira"
+//document.write(nome.toLowerCase())
+
+var nome = " Eduardo Cesar Maia Moreira Oliveira "
+document.write("-"+nome.trim()+"-") // Remove os espaços nas extremidades
