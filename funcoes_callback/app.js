cfb630fd0812ac4072e448a5ebb899a63ea81e43
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-12-14 15:03:24 -0300

    Funcão CallBack

---
+++ funcoes_callback/app.js
@@ -0,0 +1,20 @@
+function exibirArtigo(id, callbackSucesso, callbackErro) {
+
+    if (true){
+        callbackSucesso("Funcoes de callback em JS", "Funções de callback são muito utilizadas...")
+    }else{
+        callbackErro("Erro a recuperar os dados.")
+    }
+}
+
+var callbackSucesso = function(titulo, descricao) {
+    document.write("<h1>" + titulo + "</h1>")
+    document.write("<hr />")
+    document.write("<p>" + descricao + "</p>")
+}
+
+var callbackErro = function(erro) {
+    document.write("<p><b>Erro:</b>" + erro + "<p>")
+}
+
+exibirArtigo(1, callbackSucesso, callbackErro)
