4118bca8629bd6798a5b348f744a3202ba00d2dd
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-12-27 14:50:38 -0300

    Estilos de Elementos

---
+++ estilos_de_elementos/index.js
@@ -0,0 +1,5 @@
+function modificaEstilo(corDeFundo) {
+    document.getElementById('quadrado').style.backgroundColor = corDeFundo
+    document.getElementById('quadrado').style.width = 1
+    document.getElementById('quadrado').style.height = a
+}
