f843e7e85a969dca35019a0889cb069ebf55a529
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-12-28 16:12:24 -0300

    Array

---
+++ array_multidimensional/index.js
@@ -0,0 +1,8 @@
+var lista_coisas = Array();
+lista_coisas["frutas"] = Array();
+lista_coisas["frutas"][1] = "Banana";
+lista_coisas["frutas"][2] = "Maça";
+lista_coisas["frutas"][3] = "Morango";
+lista_coisas["frutas"][4] = "Uva";
+lista_coisas["frutas"][5] = "Pêra";
+console.log(lista_coisas);
